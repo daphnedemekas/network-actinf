edd3e63f0ebd7c6859b7b9ffc3dd313385f81bd9
conorheins <conor.heins@gmail.com>
2022-04-06 14:30:13 +0200

    make sure image writing happens outside of repo folder so it doesn't get committed during `git add .`

--- ising_model/demo_modified.py
+++ ising_model/demo_modified.py
@@ -1,5 +1,7 @@
 #%%
 
+import os
+from pathlib import Path
 import numpy as np 
 import networkx as nx
 from simulation import Simulation 
@@ -36,7 +38,7 @@ def run():
     sim = Simulation(G, omegas, ps_vec)
 
     phi_hist, spin_hist = sim.run_modified(T)
-    print(phi_hist.shape,phi_hist)
+    # print(phi_hist.shape,phi_hist)
     # print(spin_hist.shape,spin_hist)
 
     fig, ax = plt.subplots(1,2,figsize=(10,4),dpi=100)
@@ -48,7 +50,11 @@ def run():
     ax[1].imshow(spin_hist[:,:until_T], aspect='auto', interpolation = 'none')
     ax[1].set_title('Spin Hist')
     
-    plt.savefig( 'tmp.png', dpi=325, bbox_inches='tight')
+    write_folder = Path(__file__).parent.parent.parent / 'tmp_out'
+    if not write_folder.is_dir():
+        os.mkdir(write_folder)
+    
+    plt.savefig(os.path.join(write_folder, 'tmp.png'), dpi=325, bbox_inches='tight')
     # plt.show()
 
 if __name__ == "__main__":
