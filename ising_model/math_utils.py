9321948b46a9bc828f74a7408755a6161fb75a48
conorheins <conor.heins@gmail.com>
2022-04-12 16:07:35 +0200

    added function for computing the extra normaliizng term for the dFdk_i learning rule:
    Co-authored-by: daphne-21 <daphne-21@users.noreply.github.com>
    Co-authored-by: Brennan Klein <jkbren@users.noreply.github.com>

--- ising_model/math_utils.py
+++ ising_model/math_utils.py
@@ -6,3 +6,13 @@ EPS = 1e-16
 def log_stable(arr):
 
     return np.log(arr + EPS)
+
+def compute_exp_normalizing(omega, k):
+    """ 
+    Quick function for computing the big exponential /softmax term at the end of the update
+    """
+    omega_C = 1. - omega
+    exp_k_omega_special = np.exp(k*(1. - 2*omega))
+    numer = omega + omega_C * exp_k_omega_special
+    denom = 1. + exp_k_omega_special
+    return (2. * (numer / denom))
