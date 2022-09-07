2692a2c1037e80dbe8a055a20052110c610bfecf
daphnedemekas <daphne@9fin.com>
2022-09-07 21:47:54 +0100

    sweep learning rates

--- prisoners_dilemma/sweep2.py
+++ prisoners_dilemma/sweep2.py
@@ -83,42 +83,33 @@ def construct(precision_prosocial, precision_antisocial, lr_pB):
 
     return agent_1, agent_2, D
 
-T = 200
+T = 1000
+num_trials = 1
 
-actions_over_time_all = np.zeros((T, 2,15,15,100))
-B1_over_time_all = np.zeros((T, 4, 4, 2, 2,15,15,100))
+actions_over_time_all = np.zeros((T, 1,100,100,num_trials))
+#B1_over_time_all = np.zeros((T, 4, 4, 2, 2,30,30,num_trials))
 
-q_pi_over_time_all = np.zeros((T, 2, 2,15,15,100))
-num_trials = 100
+#q_pi_over_time_all = np.zeros((T, 2, 2,30,30,num_trials))
 
-for k, lr_pB in enumerate(np.linspace(0.0,1.0,15)):
-    print(f"lr = : {lr_pB}")
-    for j, alpha_m in enumerate(np.linspace(0.01,5.0,15)):
+for k, lr_pB_1 in enumerate(np.linspace(0.01,0.6,100)):
+    print(f"lr = : {lr_pB_1}")
+    for j, lr_pB_2 in enumerate(np.linspace(0.01,0.6,100)):
         for t in range(num_trials):
-            observation_1 = np.random.choice([0,1,2,3])
-            if observation_1 == 0:
-                observation_2 = 0
-            elif observation_1 == 1:
-                observation_2 = 2
-            elif observation_1 == 2:
-                observation_2 = 1
-            elif observation_1 == 3:
-                observation_2 = 3
 
-            alpha_1 = np.random.normal(alpha_m, 0.15)
-            alpha_2 = np.random.normal(alpha_m, 0.15)
+            #alpha_1 = np.random.normal(alpha_m, 0.15)
+            #alpha_2 = np.random.normal(alpha_m, 0.15)
 
-            agent_1, agent_2, D = construct_2(lr_pB = lr_pB,lr_pB_2 = lr_pB,factors_to_learn="all")
-            agent_1.action_selection = "stochastic"
-            agent_2.action_selection = "stochastic"
-            agent_1.alpha = alpha_1
-            agent_2.alpha = alpha_2
-            actions_over_time, B1_over_time, q_pi_over_time, qs_over_time = sweep_2(agent_1, agent_2, observation_1 = [observation_1], observation_2 = [observation_2],D=D,T=200)
+            agent_1, agent_2, D = construct_2(lr_pB = lr_pB_1,lr_pB_2 = lr_pB_2,factors_to_learn="all")
+            #agent_1.action_selection = "stochastic"
+            #agent_2.action_selection = "stochastic"
+            #agent_1.alpha = alpha_1
+            #agent_2.alpha = alpha_2
+            actions_over_time = sweep_3(agent_1, agent_2, observation_1 = [0], observation_2 = [0],D=D,T=T)
 
-            B1_over_time_all[:,:,:,:,:,k,j,t] = B1_over_time
-            q_pi_over_time_all[:,:,:,k,j,t] = q_pi_over_time
+            #B1_over_time_all[:,:,:,:,:,k,j,t] = B1_over_time
+            #q_pi_over_time_all[:,:,:,k,j,t] = q_pi_over_time
             actions_over_time_all[:,:,k,j,t] = actions_over_time
 
 np.save('actions_over_time_all',actions_over_time_all,allow_pickle = True)
-np.save('B1_over_time_all',B1_over_time_all,allow_pickle = True)
-np.save('q_pi_over_time_all',q_pi_over_time_all,allow_pickle = True)
+#np.save('B1_over_time_all',B1_over_time_all,allow_pickle = True)
+#np.save('q_pi_over_time_all',q_pi_over_time_all,allow_pickle = True)
