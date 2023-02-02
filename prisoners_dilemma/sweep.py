83adea8b94526f1efa7746702eb451346360dafa
Daphne Demekas <daphnedemekas@dhcp-10-134-232-56.uawifi.arizona.edu>
2023-02-02 13:16:06 -0700

    try w a task id

--- prisoners_dilemma/sweep.py
+++ prisoners_dilemma/sweep.py
@@ -9,8 +9,9 @@ os.mkdir("stochastic")
 def run_dual_sweep_stochastic(
     T,
     num_trials,
-    alphas=[1, 3],
-    lrs1=np.linspace(0.01, 0.6, 100)[50:],
+    task_id,
+    alpha=1,
+    lrs1=np.linspace(0.01, 0.6, 100),
     lrs2=np.linspace(0.01, 0.6, 100),
 ):
     # B1_over_time_all = np.zeros(
@@ -19,44 +20,43 @@ def run_dual_sweep_stochastic(
     # q_pi_over_time_all = np.zeros(
     #     (T, 2, 2, len(lrs1), len(lrs2), len(alphas), num_trials)
     # )
+    lr_pB_1 = lrs1[task_id]
+    k = task_id
+    if not os.path.exists(f"stochastic/{lr_pB_1}"):
+        os.mkdir(f"stochastic/{lr_pB_1}")
+    print(f"lr = : {lr_pB_1}")
+    for j, lr_pB_2 in enumerate(lrs2):
+        if os.path.exists(f"stochastic/{lr_pB_1}/{lr_pB_2}"):
+            continue
+        os.mkdir(f"stochastic/{lr_pB_1}/{lr_pB_2}")
 
-    for a, alpha in enumerate(alphas):
-        for k, lr_pB_1 in enumerate(lrs1):
-            if not os.path.exists(f"stochastic/{lr_pB_1}"):
-                os.mkdir(f"stochastic/{lr_pB_1}")
-            print(f"lr = : {lr_pB_1}")
-            for j, lr_pB_2 in enumerate(lrs2):
-                if os.path.exists(f"stochastic/{lr_pB_1}/{lr_pB_2}"):
-                    continue
-                os.mkdir(f"stochastic/{lr_pB_1}/{lr_pB_2}")
+        print(f"lr2 = : {lr_pB_2}")
 
-                print(f"lr2 = : {lr_pB_2}")
+        collect = []
+        for t in range(num_trials):
 
-                collect = []
-                for t in range(num_trials):
+            alpha_1 = np.random.normal(alpha, 0.15)
+            alpha_2 = np.random.normal(alpha, 0.15)
+            agent_1, agent_2, D = construct(
+                lr_pB=lr_pB_1, lr_pB_2=lr_pB_2, factors_to_learn="all"
+            )
+            agent_1.action_selection = "stochastic"
+            agent_2.action_selection = "stochastic"
+            agent_1.alpha = alpha_1
+            agent_2.alpha = alpha_2
+            actions_over_time = run_sim_collect_actions(
+                agent_1, agent_2, observation_1=[0], observation_2=[0], D=D, T=T
+            )
+            collect.append(actions_over_time)
 
-                    alpha_1 = np.random.normal(alpha, 0.15)
-                    alpha_2 = np.random.normal(alpha, 0.15)
-                    agent_1, agent_2, D = construct(
-                        lr_pB=lr_pB_1, lr_pB_2=lr_pB_2, factors_to_learn="all"
-                    )
-                    agent_1.action_selection = "stochastic"
-                    agent_2.action_selection = "stochastic"
-                    agent_1.alpha = alpha_1
-                    agent_2.alpha = alpha_2
-                    actions_over_time = run_sim_collect_actions(
-                        agent_1, agent_2, observation_1=[0], observation_2=[0], D=D, T=T
-                    )
-                    collect.append(actions_over_time)
-
-                actions_over_time_all = np.mean(np.array(collect), axis=0)
-                # B1_over_time_all[:, :, :, :, :, k, j, a, t] = B1_over_time
-                # q_pi_over_time_all[:, :, :, k, j, a, t] = q_pi_over_time
-                np.save(
-                    f"stochastic/{lr_pB_1}/{lr_pB_2}/actions_over_time_all",
-                    actions_over_time_all,
-                    allow_pickle=True,
-                )
+        actions_over_time_all = np.mean(np.array(collect), axis=0)
+        # B1_over_time_all[:, :, :, :, :, k, j, a, t] = B1_over_time
+        # q_pi_over_time_all[:, :, :, k, j, a, t] = q_pi_over_time
+        np.save(
+            f"stochastic/{lr_pB_1}/{lr_pB_2}/actions_over_time_all",
+            actions_over_time_all,
+            allow_pickle=True,
+        )
     # np.save("stochastic/B1_over_time_all", B1_over_time_all, allow_pickle=True)
     # np.save("stochastic/q_pi_over_time_all", q_pi_over_time_all, allow_pickle=True)
 
@@ -104,4 +104,9 @@ def run_dual_sweep_deterministic(
         np.save("q_pi_over_time_all", q_pi_over_time_all, allow_pickle=True)
 
 
-run_dual_sweep_stochastic(1000, 100)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("task_id")
+args = parser.parse_args()
+
+run_dual_sweep_stochastic(2000, 100, args.task_id)
